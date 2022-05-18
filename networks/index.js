352f1588d1ee23eeebfceadbc2738cfb8fd3953b
didi <git@d10r.net>
2022-05-18 10:13:55 +0200

    added missing startBlocks and inlined all properties to expose them to json-only consumers

--- networks/index.js
+++ networks/index.js
@@ -1,16 +1,5 @@
 const nl = require("./list.json");
 
-// add default and derived properties
-for(let i=0; i<nl.length; i++) {
-    nl[i].subgraphV1 = nl[i].subgraphV1 || {};
-    nl[i].subgraphV1.name = nl[i].subgraphV1.name || `protocol-v1-${nl[i].name}`;
-    nl[i].subgraphV1.hostedEndpoint = nl[i].subgraphV1.hostedEndpoint ||
-        `https://api.thegraph.com/subgraphs/name/superfluid-finance/${nl[i].subgraphV1.name}`;
-    
-    nl[i].startBlockV1 = nl[i].startBlockV1 || 0;
-    nl[i].logsQueryRange = nl[i].logsQueryRange || 10000;
-}
-
 module.exports = {
     networks: nl,
 
