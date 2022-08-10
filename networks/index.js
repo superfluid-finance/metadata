add26fadb4eadcdfee3de8dfed981f269407ae9f
didi <git@d10r.net>
2022-08-10 10:32:04 +0200

    index.js which can be imported as ES6 module

--- networks/index.js
+++ networks/index.js
@@ -1,21 +1,34 @@
-const nl = require("./list.json");
+//const nl = require("./list.json");
 
-module.exports = {
-    networks: nl,
+//import nl from "./list.json" assert { type: "json" };
 
-    mainnets: nl.filter(e => ! e.isTestnet),
+let _networks = [];
 
-    testnets: nl.filter(e => e.isTestnet),
+export async function init() {
+//    (_networks, await fetch('./list.json'));
+    // TODO: use relative path?
+    const nw = await (await fetch('https://raw.githubusercontent.com/superfluid-finance/metadata/master/networks/list.json')).json();
+    _networks.push(...nw);
+}
+
+export const networkHelpers = {
+    networks: _networks,
+
+    //getMainnets: () => _networks.filter(e => ! e.isTestnet),
+    mainnets: _networks.filter(e => ! e.isTestnet),
+
+    testnets: _networks.filter(e => e.isTestnet),
 
     getNetworkByChainId: function (chainId) {
-        return this.networks.filter((n) => n.chainId === chainId)[0];
+        console.log(`getting network for id ${chainId}`);
+        return _networks.filter((n) => n.chainId === chainId)[0];
     },
 
     getNetworkByName: function (name) {
-        return this.networks.filter((n) => n.name === name)[0];
+        return _networks.filter((n) => n.name === name)[0];
     },
 
     getNetworkByShortName: function (shortName) {
-        return this.networks.filter((n) => n.shortName === shortName)[0];
+        return _networks.filter((n) => n.shortName === shortName)[0];
     },
 };
