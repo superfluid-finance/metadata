293f45249a215390af3d1a4ff0443ac5f5f8842d
davinchee <0xdavinchee@gmail.com>
2022-08-11 12:25:44 +0300

    build fix

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+rm main/networks/list.js
+rm module/networks/list.js
 touch main/networks/list.js
 touch module/networks.list.js
-echo 'export default' | tee main/networks/list.js module/networks/list.js > /dev/null
+echo 'module.exports =' > main/networks/list.js
+echo 'export default' > module/networks/list.js
 cat list.json | tee -a main/networks/list.js  module/networks/list.js > /dev/null
