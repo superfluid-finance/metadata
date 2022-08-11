4bd7208a2e972f682ea1d063750d6de5a82421e3
davinchee <0xdavinchee@gmail.com>
2022-08-11 13:55:54 +0300

    fix build.sh typo

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm main/networks/list.cjs
 rm module/networks/list.js
 touch main/networks/list.cjs
-touch module/networks.list.js
+touch module/networks/list.js
 echo 'module.exports =' > main/networks/list.cjs
 echo 'export default' > module/networks/list.js
 cat list.json | tee -a main/networks/list.cjs  module/networks/list.js > /dev/null
