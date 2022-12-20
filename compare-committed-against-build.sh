47e0b1609e14e124fd6871ca248bbb695461e25d
0xdavinchee <0xdavinchee@gmail.com>
2022-12-20 13:50:08 +0200

    fix or syntax

--- compare-committed-against-build.sh
+++ compare-committed-against-build.sh
@@ -17,7 +17,7 @@ main_list_built_hash=$(hashFiles './main/networks/list.cjs')
 module_list_built_hash=$(hashFiles './module/networks/list.js')
 
 # Compare the hashes and exit if they are not equal
-if [ "$main_list_committed_hash" != "$main_list_built_hash" || "$module_list_committed_hash" != "$module_list_built_hash" ]; then
+if [ "$main_list_committed_hash" != "$main_list_built_hash"] || ["$module_list_committed_hash" != "$module_list_built_hash" ]; then
     echo "The built files are not consistent with the committed files. Please run ./build.sh and commit the changes."
     exit 1
 fi
